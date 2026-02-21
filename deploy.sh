e03a668d4a245944163e255d77fc429d05d2ea14
kamiy2743 <kamiy2743@gmail.com>
2026-02-21 20:46:53 +0900

    nginxを使ってページの表示まで

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SRC="/home/kamiy2743/workspace/http-server/public/"
+DST="/var/www/http-server/"
+
+NGINX_SRC="/home/kamiy2743/workspace/http-server/nginx/http-server.conf"
+NGINX_AVAIL="/etc/nginx/sites-available/http-server.conf"
+NGINX_ENABLED="/etc/nginx/sites-enabled/http-server.conf"
+
+mkdir -p $DST
+rsync -av --delete $SRC $DST
+
+install -m 644 $NGINX_SRC $NGINX_AVAIL
+ln -sfn $NGINX_AVAIL $NGINX_ENABLED
+
+nginx -t
+systemctl reload nginx
